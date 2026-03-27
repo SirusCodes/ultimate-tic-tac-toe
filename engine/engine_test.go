3e0e497cc7e20863b3699f96129774456163980b
SirusCodes <hey@darshanrander.com>
2026-03-27 23:12:16 +0530

    test: add engine benchmarks

---
+++ engine/engine_test.go
@@ -0,0 +1,21 @@
+package engine_test
+
+import (
+	"testing"
+
+	"github.com/SirusCodes/9x9-analysis/engine"
+	"github.com/SirusCodes/9x9-analysis/game"
+	"github.com/SirusCodes/9x9-analysis/player"
+)
+
+func BenchmarkRunEngine_Depth10(b *testing.B) {
+	b.ReportAllocs()
+	depth := uint8(10)
+
+	b.ResetTimer()
+
+	for b.Loop() {
+		g := game.NewGame(player.NewPlayer(0, 0), player.NewPlayer(0, 0), 0)
+		engine.RunEngine(g, depth)
+	}
+}
