7d32f938d87098870aef6ad7aeb02442d49bd341
SirusCodes <hey@darshanrander.com>
2026-03-31 14:03:09 +0530

    minor: get run time

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/SirusCodes/ultimate-tic-tac-toe/engine"
 	"github.com/SirusCodes/ultimate-tic-tac-toe/game"
@@ -9,13 +10,13 @@ import (
 )
 
 func main() {
-	g := game.NewGame(player.NewPlayer(0, 0), player.NewPlayer(0, 0), 0)
-	g.PlayMove(game.Move{BoardZone: 4, Position: 4})
-	g.PlayMove(game.Move{BoardZone: 4, Position: 7})
-	g.PlayMove(game.Move{BoardZone: 7, Position: 4})
-	result := engine.RunEngine(g, 10)
 
-	fmt.Printf("%+v\n", len(result.AllMoves))
+	start := time.Now()
+	g := game.NewGame(player.NewPlayer(0, 0), player.NewPlayer(0, 0), 0)
+	result := engine.RunEngine(g, 10)
+	fmt.Printf("Ran for %fs\n", time.Since(start).Seconds())
+
+	fmt.Printf("moves: %+v\n", len(result.AllMoves))
 	fmt.Printf("best: %+v\n", result.BestMove)
 	fmt.Printf("generated states: %d\n", result.StateChecks)
 }
